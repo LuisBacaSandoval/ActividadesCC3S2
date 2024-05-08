eb31b34226419e0436faef7b24e4f1bf52f8d608
LuisBacaSandoval <luis.baca.s@uni.pe>
2024-05-07 23:47:43 -0500

    edit Actividad3-AAA

--- Actividad3-AAA/src/main/java/org/example/Calculator.java
+++ Actividad3-AAA/src/main/java/org/example/Calculator.java
@@ -1,22 +1,30 @@
 package org.example;
 
+// Definición de la clase Calculator
 public class Calculator {
+
+    // Método para sumar dos números enteros
     public int sumar(int numeroA, int numeroB) {
-        return numeroA + numeroB;
+        return numeroA + numeroB; // Retorna la suma de numeroA y numeroB
     }
 
+    // Método para restar dos números enteros
     public int restar(int numeroA, int numeroB) {
-        return numeroA - numeroB;
+        return numeroA - numeroB; // Retorna la resta de numeroA menos numeroB
     }
 
+    // Método para multiplicar dos números enteros
     public int multiplicacion(int numeroA, int numeroB) {
-        return numeroA * numeroB;
+        return numeroA * numeroB; // Retorna el producto de numeroA y numeroB
     }
 
+    // Método para dividir dos números enteros
     public double division(int numeroA, int numeroB) {
+        // Comprueba si el denominador es cero para evitar división por cero
         if (numeroB == 0) {
-            throw new ArithmeticException("Division por cero");
+            throw new ArithmeticException("Division por cero"); // Lanza una excepción si es cero
         }
+        // Realiza la división y retorna el resultado como un double para permitir decimales
         return (double) numeroA / numeroB;
     }
 }
